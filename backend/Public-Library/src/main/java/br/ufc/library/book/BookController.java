3447e535950ad80db05d7b3496bbd61561c44513
fernanddo-s <fernando.osousa@alu.ufc.br>
2021-07-23 22:47:28 -0300

    add endpoint books/update

--- backend/Public-Library/src/main/java/br/ufc/library/book/BookController.java
+++ backend/Public-Library/src/main/java/br/ufc/library/book/BookController.java
@@ -1,15 +1,19 @@
 package br.ufc.library.book;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,4 +51,36 @@ public class BookController {
     public List<Book> getAllBooks(){
     	return bookRepository.findAll();
     }
+    
+    @PutMapping(value = "update/{id}")
+    public ResponseEntity<Object> updateBook(@RequestBody Book book, @PathVariable long id){
+    	Optional<Book> BookRepository = Optional.ofNullable(bookRepository.findById(id));
+
+	    if (!BookRepository.isPresent()) {
+	        return ResponseEntity.notFound().build();
+	    }
+	    
+	    book.setId(id);
+	
+	    bookRepository.save(book);
+	
+	    return ResponseEntity.noContent().build();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
